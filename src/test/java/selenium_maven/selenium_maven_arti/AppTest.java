eadd8934fd03d5e5e88290a7197680be8fa79fe1
nihar080 <sahu.nihar80@gmail.com>
2023-06-05 18:57:26 +0530

    Fist commit for maven project

---
+++ src/test/java/selenium_maven/selenium_maven_arti/AppTest.java
@@ -0,0 +1,24 @@
+package selenium_maven.selenium_maven_arti;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	WebDriver driver  = new ChromeDriver();
+    	driver.get("https://www.google.com");
+        assertTrue( true );
+    }
+}
